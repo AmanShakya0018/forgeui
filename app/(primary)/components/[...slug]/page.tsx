83a9a5ebcc45d57a6453aac2cd82e458e422c5b1
AmanShakya0018 <amanshakya1808@gmail.com>
2024-12-29 14:58:16 +0530

    Slug page ui changes

--- app/(primary)/components/[...slug]/page.tsx
+++ app/(primary)/components/[...slug]/page.tsx
@@ -29,6 +29,11 @@ export default async function ItemPage({ params }: ItemPageProps) {
 
   return (
     <article className="container lg:pl-8 py-6 prose dark:prose-invert max-w-6xl mx-auto">
+      <div className="mb-4 flex items-center space-x-1 text-sm leading-none text-gray-400">
+        <div className="truncate">Components</div>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-right h-3.5 w-3.5"><path d="m9 18 6-6-6-6"></path></svg>
+        <div className="text-foreground">{item.title}</div>
+      </div>
       <h1 className="mb-2 text-4xl font-extrabold">{item.title}</h1>
       {item.description ? (
         <p className="text-lg mt-0 text-gray-400">{item.description}</p>
