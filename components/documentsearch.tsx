89e1446c36a730bba8d380fa70249a95c3f83e66
AmanShakya0018 <amanshakya1808@gmail.com>
2025-01-13 20:46:29 +0530

    added the components search functionality

--- components/documentsearch.tsx
+++ components/documentsearch.tsx
@@ -1,45 +1,81 @@
 "use client"
-import { useToast } from '@/hooks/use-toast';
 import React from 'react';
 
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "@/components/ui/command"
+import { primaryItems } from '@/contants';
+
 const DocumentSearch = () => {
+  const sortedPrimaryItems = [...primaryItems].sort((a, b) => a.name.localeCompare(b.name));
+  const [open, setOpen] = React.useState(false)
 
-  const { toast } = useToast()
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault()
+        setOpen((open) => !open)
+      }
+    }
 
-  const handleClick = () => {
-    toast({
-      title: 'Available soon',
-      description: 'This feature is in development and will be available soon.'
-    });
-  }
+    document.addEventListener("keydown", down)
+    return () => document.removeEventListener("keydown", down)
+  }, [])
+
+
 
   return (
-    <button
-      className="flex relative justify-start items-center text-sm text-zinc-500 dark:text-zinc-400 dark:border-white/[0.2] py-2 w-fit border border-transparent shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] px-4 rounded-xl bg-white/50 dark:bg-black/50"
-      onClick={handleClick}
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="h-4 w-4 text-neutral-500"
+    <>
+      <button
+        className="flex relative justify-start items-center text-sm text-zinc-500 dark:text-zinc-400 dark:border-white/[0.2] py-2 w-fit border border-transparent shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] px-4 rounded-xl bg-white/50 dark:bg-black/50"
+        onClick={() => setOpen(true)}
       >
-        <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
-        <path d="M21 21l-6 -6" />
-      </svg>
-      <span className="transition-colors hover:text-foreground/80 text-foreground/60 text-xs sm:text-sm font-medium pl-2 pr-4">
-        Search <span className="hidden xl:inline-block">Components</span>
-      </span>
-      <kbd className="pointer-events-none hidden h-5 select-none items-center gap-1 rounded border bg-zinc-100 dark:bg-zinc-800 px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
-        <span className="text-xs">⌘</span>K
-      </kbd>
-    </button>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="h-4 w-4 text-neutral-500"
+        >
+          <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+          <path d="M21 21l-6 -6" />
+        </svg>
+        <span className="transition-colors hover:text-foreground/80 text-foreground/60 text-xs sm:text-sm font-medium pl-2 pr-4">
+          Search <span className="hidden xl:inline-block">Components</span>
+        </span>
+        <kbd className="pointer-events-none hidden h-5 select-none items-center gap-1 rounded border bg-zinc-100 dark:bg-zinc-800 px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+          <span className="text-xs">⌘</span>K
+        </kbd>
+      </button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="All Components">
+            {sortedPrimaryItems.map((item) => (
+              <a href={item.href} key={item.name}>
+                <CommandItem >
+                  {item.name}
+                </CommandItem>
+              </a>
+            ))}
+
+          </CommandGroup>
+          <CommandSeparator />
+        </CommandList>
+      </CommandDialog>
+    </>
   );
 };
 
