d2ca95c0fa4f1c226f89f530ec55623cf57353b3
AmanShakya0018 <amanshakya1808@gmail.com>
2025-06-05 14:00:47 +0530

    chore: added statsio analytics script

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import Provider from "@/provider/provider";
 import { Toaster } from "@/components/ui/toaster";
 import { Analytics } from "@vercel/analytics/react"
+import Script from "next/script";
 
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
@@ -20,6 +21,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-pt-[4.2rem]" suppressHydrationWarning={true}>
+      <Script
+        defer
+        data-site="217027c0-3557-4a81-8f84-92043de74a6a"
+        src="https://statsio.amanshakya.in/tracker.js"
+      />
       <body
         className={cn("min-h-screen font-sans antialiased bg-background dark:bg-black ", inter.variable)}
       >
